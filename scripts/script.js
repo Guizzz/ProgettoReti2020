f84d6c192d176f2f5ec47f9bd208663a697a2fd4
Alessio Caperna <caperna.1810609@studenti.uniroma1.it>
2020-04-24 12:02:46 +0200

    v

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function enter(){
+  $("#entry").css("transform","translate(-200vh)");
+  // $("#entry").css("visibility", "hidden");
+  $("#dark-bg").css("transform", "translate(0)");
+  $("#dark-bg").css("visibility", "visible");
+  $("#site").css("visibility", "visible");
+  $("#site").css("overflow", "visible");
+  $("#site").css("max-height", "auto");
+
+}
+
+function log(){
+  console.log("ciao");
+}
