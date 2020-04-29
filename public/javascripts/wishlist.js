ef79f0be7366eca028d97dbc1c1a3cc9b15da686
Alessio Caperna <caperna.1810609@studenti.uniroma1.it>
2020-04-29 16:08:41 +0200

    v1

---
+++ public/javascripts/wishlist.js
@@ -0,0 +1,30 @@
+function trigger(){
+  var val;
+  var input = document.getElementById("input");
+  var todo = parseInt(document.getElementById("todo").innerHTML);
+  if(input){
+    input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      if(input.value){
+        console.log("ciao");
+        val = input.value.toUpperCase();
+        input.value = "";
+        document.getElementById("lista").innerHTML += "<li>"+val+"</li>";
+        todo+=1;
+        document.getElementById("todo").innerHTML = todo;
+        console.log(document.getElementById("lista").length);
+        }
+      }
+    });
+  }
+}
+
+function viewLabel(){
+  $("#label").css("visibility", "visible");
+}
+
+
+
+function log(){
+  console.log("ciao");
+}
