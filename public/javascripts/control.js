283eb56238391db0824557b9e3170fc6d598450a
Alessio Caperna <caperna.1810609@studenti.uniroma1.it>
2020-07-19 13:31:57 +0200

    reset

--- public/javascripts/control.js
+++
@@ -1,8 +0,0 @@
-function validaForm(){
-    if(document.form_registrazione.password1.value != document.form_registrazione.password2.value){
-        alert("Le password non combaciano");
-        return false;
-    }
-    else
-        return true;
-}
--- routes/contatti.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET pagina contatti. */
-router.get('/', function(req, res, next) {
-  res.render('contatti',{cookie: req.cookies['username']});
-});
-
-module.exports = router;
